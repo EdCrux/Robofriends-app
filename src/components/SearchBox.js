25ab02b50b9219c1582c0d83bdac893f8fb4c4d1
Eduardo <jecruxiz@gmail.com>
2020-07-22 15:24:10 -0500

    :bug: Quit unnecesary console.logs, add aria-label for improve accesibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 
 const SearchBox = ({searchChange}) => {
-  console.log('SearchBox')
   return(
     <div>
       <input
+        aria-label="Search RoboFriends"
         className='pa3 ba b--green bg-lighttest-blue'
         type='text'
         placeholder='Search for a robot'
