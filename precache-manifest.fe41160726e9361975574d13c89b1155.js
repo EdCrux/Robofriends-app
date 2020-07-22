601cd8868ec9db2fe5e8a70020191728f5fe1483
Eduardo <jecruxiz@gmail.com>
2020-07-22 14:55:54 -0500

    Updates

---
+++ precache-manifest.fe41160726e9361975574d13c89b1155.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9fd145f87515fa08c84b9d98c9d762",
+    "url": "/robofriends-app/index.html"
+  },
+  {
+    "revision": "f9051827e7eab74781cb",
+    "url": "/robofriends-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "59a1b128d1bef1a783a4",
+    "url": "/robofriends-app/static/css/main.d47a607c.chunk.css"
+  },
+  {
+    "revision": "f9051827e7eab74781cb",
+    "url": "/robofriends-app/static/js/2.2511259c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends-app/static/js/2.2511259c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a1b128d1bef1a783a4",
+    "url": "/robofriends-app/static/js/main.a9a26b29.chunk.js"
+  },
+  {
+    "revision": "4605bbe5457f31ef4dc5",
+    "url": "/robofriends-app/static/js/runtime-main.a2716304.js"
+  }
+]);
